22c6611bcfa049510190f4c51bb193dddb8d0135
Vitto <vitto.torresq@pucp.edu.pe>
2019-03-30 10:58:35 -0500

    gg

--- new1.cpp
+++ new1.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 #include <cmath>
-
+using namespace std;
 int main(){
 	int i=1;
 	while(i<101){
-		if((i % 3 )==0)
-	
-	
+		if((i % 15 )==0) cout<<"FizzBuzz; ";
+		else if((i % 3 )==0) cout<<"Fizz; ";
+		else if((i % 5 )==0) cout<<"Buzz; ";
+		else cout<<i<<"; ";
+	i++;
 	}
-	
-	
-	
-	
-	
-	
-	return 0;	
-	}	
+
+
+	return 0;
+	}
