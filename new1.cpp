7be8bd2b3e7c3b859a83390015388e0560320da5
Vitto <vitto.torresq@pucp.edu.pe>
2019-03-30 11:26:01 -0500

    gg

--- new1.cpp
+++ new1.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
 #include <cmath>
-#define MAX 100
+
 using namespace std;
 int main(){
+    const int max=100;
     int B[max];
     int F[max];
+    int Q[max];
     int N[max];
-	int b=1,n=1,f=1;
+	int b=0,n=0,f=0,q=0,i=1;
 
-	while(i<101){
+	while(n<101){
+        if((n%15)==0){
+            Q[q]=i;
+            q++;
+        }
+        else if((n%5)==0){
+            B[b]=i;
+            b++;
+        }
+        else if((n%15)==0){
+            Q[f]=i;
+            f++;
+        }
+        else{
+            N[n]=i;
+            n++;
+        }
+        i++;
 
 	}
-
+        cout<<B<<endl;
+        cout<<F<<endl;
+        cout<<Q<<endl;
+        cout<<N<<endl;
 
 	return 0;
 	}
